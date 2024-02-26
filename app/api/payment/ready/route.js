da61d5c0782fbc22c8faf8916c172f3badaffce8
inspirit941 <inspirit941@gmail.com>
2024-02-26 10:14:45 +0900

    feat: axios로 request 모듈 만들어서 문제 해결 - unsafe legacy renegotiation disabled - SSL 통신 안되는 이슈

--- app/api/payment/ready/route.js
+++ app/api/payment/ready/route.js
@@ -1,31 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
+import { postRequest } from '../../../utils/request'
 
 export async function POST(request) {
   const payload = request.json();
 
   const url = "https://open-api.kakaopay.com/online/v1/payment/ready";
   const data = {
-    cid: process.env.SINGLE_PAYMENT_CID,
+    cid: process.env.NEXT_PUBLIC_SINGLE_PAYMENT_CID,
     partner_order_id: "partner_order_id",
     partner_user_id: "partner_user_id",
     item_name: "테스트",
     quantity: "1",
     total_amount: "2200",
     vat_amount: "200",
-    tax_fee_amount: "0",
+    tax_free_amount: "0",
     approval_url: "https://developers.kakao.com/success",
     fail_url: "https://developers.kakao.com/fail",
     cancel_url: "https://developers.kakao.com/cancel",
   };
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      Authorization: `SECRET_KEY ${process.env.NEXT_PUBLIC_SECRET_KEY_DEV}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  const result = await response.json();
-  console.log(result);
-  return result;
+  const response = await postRequest(url, {
+    Authorization: `SECRET_KEY ${process.env.NEXT_PUBLIC_SECRET_KEY_DEV}`,
+    "Content-Type": "application/json",
+  }, JSON.stringify(data))
+
+  const result = await response;
+  console.log(result.data);
+  return NextResponse.json(result.data)
+  // return result;
 }
---
+++ app/utils/request.js
@@ -0,0 +1,28 @@
+import crypto from 'crypto';
+import https from 'https';
+import axios from "axios";
+
+/**
+ * Handle this problem with Node 18
+ * write EPROTO B8150000:error:0A000152:SSL routines:final_renegotiate:unsafe legacy renegotiation disabled
+ * see https://stackoverflow.com/questions/74324019/allow-legacy-renegotiation-for-nodejs/74600467#74600467
+ **/
+const allowLegacyRenegotiationforNodeJsOptions = {
+  httpsAgent: new https.Agent({
+    // for self signed you could also add
+    // rejectUnauthorized: false,
+    // allow legacy server
+    secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+  }),
+};
+
+export async function postRequest(url, headers, data) {
+  const response =  axios({
+    ...allowLegacyRenegotiationforNodeJsOptions,
+    url,
+    headers: headers,
+    method: 'POST',
+    data: data,
+  });
+  return response
+}
